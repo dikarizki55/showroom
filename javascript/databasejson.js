3caf98386914e3f72f272be9c6b43edcc165667f
Dika Rizki <dikaesmidorikawa55@gmail.com>
2023-09-02 22:21:19 +0700

    Adding Animation

--- javascript/databasejson.js
+++ javascript/databasejson.js
@@ -2,17 +2,28 @@ const databasejson = [
     {
         nama:"swanson red",
         image:["./assets/img/a.webp","./assets/img/b.jpg","./assets/img/c.jpg"],
-        color:"red"
+        color:"#c28f5d",
+        overview:"Bring classic style to your kitchen or dining room. Crafted from FSC®-certified solid American beech wood,1",
+        dimension:[
+            `Width: 21-3/4"`,
+            `Height: 30-2/3"`,
+           ` Depth: 21-1/4"`,
+           ` Weight: 14 lbs.`,
+            `Seat Depth: 17"`,
+           ` Seat Height: 18"`,
+           ` Back Rest Height: 13-3/5"`,
+        ],
+        loadibility: `Hc40': 100`
     },
     {
         nama:"swanson blue",
         image:["./assets/img/b.jpg","./assets/img/c.jpg","./assets/img/a.webp"],
-        color:"blue"
+        color:"#c2bf5d",
     },
     {
         nama:"swanson green",
         image:["./assets/img/c.jpg","./assets/img/a.webp","./assets/img/b.jpg"],
-        color:"green"
+        color:"#6e5448",
     }
 ]
 
--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,69 @@
 import databasejson from "./databasejson.js";
-console.log(databasejson)
+// console.log(databasejson)
 
+// colorbutton iteration
+function colorIteration() {
+    let colorBtnAll=``;
+
+    databasejson.forEach(color);
+    function color(item, index){
+        let active = ""
+        if(index==0){
+            active="active";
+        };
+        const colorBtn = `<button class="colorChange ${active}" data-value="${index}" onclick="activeSelector(this)" style="background-color: ${item.color};"></button>`
+
+        colorBtnAll += colorBtn;
+    };
+    document.getElementsByClassName('colorselector')[0].innerHTML = colorBtnAll;
+}
+
+colorIteration();
+
+
+
+
+// data change when click color
 function colorChange(i){
-let carouselItem = ``;
-let carouselButton = ``;
 
-databasejson[i].image.forEach(imageIteration);
+    // IMAGE ITERATION
+    let carouselItem = ``;
+    let carouselButton = ``;
+    databasejson[i].image.forEach(imageIteration);
+    function imageIteration(item, index){
+        let active = ""
+        if(index==0){
+        active="active";
+        }
+        const carouselItemInput = `<div class="carousel-item ${active}">
+                                <div style="background-image: url('${item}')" class="d-block w-100"></div>
+                            </div>`;
+        const carouselButtonInput = `<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${index}" class="${active}" aria-label="Slide ${index+1}">
+                                <div style="background-image: url('${item}')"></div>
+                                </button>`;
 
-function imageIteration(item, index){
-    let active = ""
-    if(index==0){
-    active="active";
+        carouselItem += carouselItemInput;
+        carouselButton += carouselButtonInput;     
+    };
+    document.getElementsByClassName("carousel-inner")[0].innerHTML = carouselItem;
+    document.getElementsByClassName("carousel-indicators")[0].innerHTML = carouselButton;
+
+
+    // OVERVIEW=
+    document.querySelector("#overview div").innerHTML = databasejson[0].overview;
+
+    // DIMENSION
+    let dimensionAll = ``;
+    databasejson[0].dimension.forEach(dimensionIteration);
+    function dimensionIteration(item,index){
+        const dimension = `<li>${item}</li>`;
+        dimensionAll += dimension;
     }
-    const carouselItemInput = `<div class="carousel-item ${active}">
-                            <div style="background-image: url('${item}');" class="d-block w-100"></div>
-                        </div>`;
-    const carouselButtonInput = `<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${index}" class="${active}" aria-label="Slide ${index+1}">
-                            <div style="background-image: url('${item}');"></div>
-                            </button>`;
+    document.querySelector("#dimension div").innerHTML = dimensionAll;
 
-    carouselItem += carouselItemInput;
-    carouselButton += carouselButtonInput;     
-};
-document.getElementsByClassName("carousel-inner")[0].innerHTML = carouselItem;
-document.getElementsByClassName("carousel-indicators")[0].innerHTML = carouselButton;
+    // LOADIBILITY
+    document.querySelector("#loadibility div").innerHTML = databasejson[0].loadibility;
+
 };
 
 colorChange(0);
@@ -31,6 +71,44 @@ colorChange(0);
 var elements = document.getElementsByClassName("colorChange");
 
 for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', function(e){colorChange(e.srcElement.dataset.value)});
+    elements[i].addEventListener('click', function(e){
+        transitionOut("transitionOpacity")
+        setTimeout(setColorChange, 300)
+        function setColorChange(){
+            // colorChange(e.srcElement.dataset.value)
+            colorChange(e.srcElement.dataset.value)
+            transitionIn("transitionOpacity");
+        }
+    });
+
 }
       
+
+// transition
+
+function transitionIn(domClass){
+    const elements = document.getElementsByClassName(domClass);
+    setTimeout(changeOpacity,10);
+    function changeOpacity(){
+        for(let i = 0; i < elements.length; i++){
+            elements[i].style.opacity = 1; 
+        };
+    }
+}
+
+function transitionOut(domClass){
+    const elements = document.getElementsByClassName(domClass);
+    setTimeout(changeOpacity,10);
+    function changeOpacity(){
+        for(let i = 0; i < elements.length; i++){
+            elements[i].style.opacity = 0; 
+        };
+    }
+}
+
+setTimeout(start,300)
+function start(){
+    transitionIn("body");
+    transitionIn("transitionOpacity");
+}
+
